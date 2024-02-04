56f7fb60d2d95bb9014549d2a8495416b20dfecd
EduardoRag <eduardojarek66@gmail.com>
2024-02-04 20:58:45 -0300

    UPDATE: .env added

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mongo_user = os.getenv('MONGO_USER')
+mongo_password = os.getenv('MONGO_PASS')
 
 def get_database():
-    connection_string = "mongodb+srv://eduardo:Ace2707093096@web-scraper.csutg7h.mongodb.net/"
+    connection_string = f"mongodb+srv://{mongo_user}:{mongo_password}@web-scraper.csutg7h.mongodb.net/"
     client = MongoClient(connection_string)
 
     return client['websites']
